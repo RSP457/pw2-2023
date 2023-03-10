d2f91fc98d5fabd0af04e0e78db34c8ae797a299
Rian Satria Permana <114471084+RSP457@users.noreply.github.com>
2023-03-10 23:32:27 +0700

    Update form_daftar.php

--- Pertemuan 2/Tugas 1/form_daftar.php
+++ Pertemuan 2/Tugas 1/form_daftar.php
@@ -1,36 +1,40 @@
-<!-- Task 1
-	Rapihkan dan buatlah form agar bisa mengirim data
-	Gunakan form request POST
--->
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Form Pendaftaran</title>
 	<!-- Load Bootstrap CSS from CDN -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<style>
+		body {
+            margin:10px	;
+            background-color:grey;
+            color:#fff;
+        }
+	</style>
 </head>
 <body>
 
 	<div>
-		<h1>Form Pendaftaran</h1>
-		<form methode="POST" action="">
+		<h1 class="text-center mt-3 text-light">Form Pendaftaran</h1>
+		<form method="POST" action="hasil_daftar.php">
 			<div class="form-group">
 				<label for="nama_lengkap">Nama Lengkap</label>
-				<input type="text" class="form-control" id="nama_lengkap">
+				<input type="text" name="nama_lengkap" class="form-control" id="nama_lengkap">
 			</div>
 			<div class="form-group">
 				<label for="email">Email</label>
-				<input type="email" class="form-control" id="email">
+				<input type="email" name="email" class="form-control" id="email">
 			</div>
 			<div class="form-group">
 				<label for="alamat">Alamat</label>
-				<textarea class="form-control" id="alamat"></textarea>
+				<textarea class="form-control" name="alamat" id="alamat"></textarea>
 			</div>
 			<div class="form-group">
 				<label for="telepon">Telepon</label>
-				<input type="tel" class="form-control" id="telepon">
+				<input type="tel" name="telepon" class="form-control" id="telepon">
 			</div>
-			<button class="btn btn-primary">Submit</button>
+			<button class="btn btn-primary" name="submit" value="submit">Submit</button>
 		</form>
 	</div>
 </body>
